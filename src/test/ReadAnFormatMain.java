86d1d3b2db4d98088cb55b097a9bae255a9bc590
心如止水 <zen96285@gmail.com>
2019-10-15 15:35:17 +0800

    实现将List数据转换为Map

--- src/test/ReadAnFormatMain.java
+++ src/test/ReadAnFormatMain.java
@@ -6,11 +6,20 @@ import app.model.TomatoTask;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+
+import java.time.LocalDate;
+import java.util.HashMap;
 
 public class ReadAnFormatMain {
 
     private TomatoTaskDataJson tomatoTaskDataJson;
+
     private ObservableList<TomatoTask> TOMATO_TASKS = FXCollections.observableArrayList();
+
+    private ObservableMap<LocalDate,ObservableList<TomatoTask>> TOMATO_TASKS_MAP =
+        FXCollections.observableMap(new HashMap<>());
+
     private final String  JSON_PATH = "res\\json\\tomatoTaskData.json";
 
     private void intiTomatoTaskData() {
@@ -19,8 +28,21 @@ public class ReadAnFormatMain {
     }
 
     public static void main(String[] args) {
+        ReadAnFormatMain readAnFormatMain = new ReadAnFormatMain();
+        readAnFormatMain.intiTomatoTaskData();
+        System.out.println(readAnFormatMain.TOMATO_TASKS);
+        readAnFormatMain.convertToMap();
+        System.out.println(readAnFormatMain.TOMATO_TASKS_MAP);
 
+    }
 
+    private void convertToMap() {
+        TOMATO_TASKS.forEach((e)->{
+            if(TOMATO_TASKS_MAP.get(e.getDate()) == null){
+                TOMATO_TASKS_MAP.put(e.getDate(), FXCollections.observableArrayList());
+            }
+            TOMATO_TASKS_MAP.get(e.getDate()).add(e);
+        });
     }
 
 }

5db2ef8c668ca9bb5f114b5fa0486c4b2ccd59d5
心如止水 <zen96285@gmail.com>
2019-10-24 14:59:32 +0800

    性能优化

--- src/app/TextWrapCell.java
+++ src/app/TextWrapCell.java
@@ -17,16 +17,17 @@ public class TextWrapCell<T> extends TableCell<T, String> {
     @Override
     public void updateItem(String item, boolean empty) {
         super.updateItem(item, empty);
-        System.out.print("updateItem:item -> ");
         if (!isEmpty()) {
             if (textControl == null) {
                 this.setWrapText(true);
                 textControl = new Text(item);
                 setGraphic(textControl);
                 textControl.textProperty().addListener((observable, oldText, newText) -> {
-                    Platform.runLater(() -> {
-                        getTableView().refresh();
-                    });
+                    if(!oldText.equals(newText)){
+                        Platform.runLater(() -> {
+                            getTableView().refresh();
+                        });
+                    }
                 });
                 wrap();
                 getTableColumn().prefWidthProperty().addListener((observable, oldValue, nameColumnNewWidth) -> {

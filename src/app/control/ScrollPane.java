268d5ddb9b54387dc9160ef0fb644831b30fb30d
心如止水 <zen96285@gmail.com>
2019-10-25 10:41:45 +0800

    追加

--- src/app/control/ScrollPane.java
+++ src/app/control/ScrollPane.java
@@ -6,6 +6,10 @@ import javafx.collections.ObservableList;
 public class ScrollPane extends  javafx.scene.control.ScrollPane{
     private ObservableList<TomatoTask> items = null;
 
+    public ObservableList<TomatoTask> getItems() {
+        return items;
+    }
+
     public void setItems(ObservableList<TomatoTask> observableList) {
         items = observableList;
     }

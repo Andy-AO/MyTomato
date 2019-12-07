6e935bc6fa6788d2ea64454c577c2963808a739d
心如止水 <zen96285@gmail.com>
2019-12-07 11:32:39 +0800

    解决空指针异常问题

--- src/app/control/GridPane.java
+++ src/app/control/GridPane.java
@@ -1,12 +1,13 @@
 package app.control;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 
 public class GridPane<DataType,NodeType extends Node> extends javafx.scene.layout.GridPane {
     protected ObservableList<DataType> items;
 
-    private ObservableList<GirdColumn<DataType,NodeType>> columns;
+    private ObservableList<GirdColumn<DataType,NodeType>> columns = FXCollections.observableArrayList();
 
     public void setItems(ObservableList<DataType> items) {
         this.items = items;

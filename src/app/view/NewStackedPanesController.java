b3559e889f6a856b15e95db0ec78950f18c3d917
心如止水 <zen96285@gmail.com>
2019-12-07 08:51:21 +0800

    新增类NewStackedPanesController

---
+++ src/app/view/NewStackedPanesController.java
@@ -0,0 +1,70 @@
+package app.view;
+
+import app.Main;
+import app.control.mytomato.StackedPanes;
+import app.model.TomatoTask;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.time.LocalDate;
+
+public class NewStackedPanesController extends Controller {
+
+
+//--------------------------------------- Field
+
+    private StackedPanes stackedPanes;
+
+//--------------------------------------- Getter Setter
+//--------------------------------------- Method
+
+    public NewStackedPanesController() {
+
+    }
+
+    @Override
+    public void setMainAndInit(Main main) {
+        super.setMainAndInit(main);
+        stackedPanes.setItemsMap(main.getTomatoTasksMap());
+        writeWhenItemsChange();
+        stackedPanes.setOnKeyPressed(this::handleTableDeleteKey);
+        stackedPanes.setStackedPanesController(this);
+    }
+
+    private void handleTableDeleteKey(KeyEvent keyEvent) {
+        KeyCode keyCode = keyEvent.getCode();
+        String keyName = keyCode.getName();
+        switch (keyName) {
+            case "Delete":
+                main.getMainLayoutController().handleDeleteButton();
+                break;
+            case "R":
+                main.getMainLayoutController().showCellHeight();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void writeWhenItemsChange() {
+        stackedPanes.getItemsMap().addListener(new MapChangeListener<LocalDate, ObservableList<TomatoTask>>() {
+            @Override
+            public void onChanged(Change<? extends LocalDate, ? extends ObservableList<TomatoTask>> change) {
+                main.getTomatoTaskDataMapJson().write();
+            }
+        });
+        stackedPanes.titledPaneItemsChangeProperty().addListener((observable, oldChange, newChange) -> {
+            main.getTomatoTaskDataMapJson().write();
+        });
+    }
+
+    public StackedPanes createScrollPane() {
+        stackedPanes = new StackedPanes();
+        return stackedPanes;
+    }
+
+
+}

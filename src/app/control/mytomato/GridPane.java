7129fbe43ecb1ff841893479f1736798578a2c7f
心如止水 <zen96285@gmail.com>
2019-12-07 11:45:11 +0800

    实现Start/End/Name三列均通过GridColumn显示

--- src/app/control/mytomato/GridPane.java
+++ src/app/control/mytomato/GridPane.java
@@ -10,17 +10,26 @@ import javafx.scene.text.Text;
 import java.util.ArrayList;
 
 public class GridPane extends app.control.GridPane<TomatoTask,Text> {
+
     private GirdColumn<TomatoTask, Text> nameColumn;
+    private GirdColumn<TomatoTask, Text> startColumn;
+    private GirdColumn<TomatoTask, Text> endColumn;
 
     public void setItems(ObservableList<TomatoTask> items) {
+
+        startColumn = new GirdColumn<>("start");
+        startColumn.setNodeFactory(data -> new Text(data.getStartTimeString()));
+        this.getColumns().add(startColumn);
+
+        endColumn = new GirdColumn<>("end");
+        endColumn.setNodeFactory(data -> new Text(data.getEndTimeString()));
+        this.getColumns().add(endColumn);
 
         nameColumn = new GirdColumn<>("name");
         nameColumn.setNodeFactory(data -> new Text(data.getName()));
         this.getColumns().add(nameColumn);
-
+        
         super.setItems(items);
 
     }
-
-
 }

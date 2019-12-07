d9557f846906bca109c101f43a87f96b10b38acf
心如止水 <zen96285@gmail.com>
2019-12-07 11:02:09 +0800

    在Control包下,简历GridPane和GirdColumn两个泛型类

---
+++ src/app/control/GirdColumn.java
@@ -0,0 +1,25 @@
+package app.control;
+
+import javafx.scene.Node;
+
+public class GirdColumn<DataType,NodeType extends Node> {
+    private final String name;
+    private GirdColumnFactory nodeFactory;
+
+    public GirdColumnFactory getNodeFactory() {
+        return nodeFactory;
+    }
+
+    public void setNodeFactory(GirdColumnFactory nodeFactory) {
+        this.nodeFactory = nodeFactory;
+    }
+
+    public GirdColumn(String name) {
+        this.name = name;
+    }
+}
+
+@FunctionalInterface
+interface GirdColumnFactory{
+   public Node generateNode();
+}
---
+++ src/app/control/GridPane.java
@@ -0,0 +1,22 @@
+package app.control;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+
+public class GridPane<DataType,NodeType extends Node> extends javafx.scene.layout.GridPane {
+    private ObservableList<DataType> items;
+
+    private ObservableList<GirdColumn<DataType,NodeType>> columns;
+
+    public void setItems(ObservableList<DataType> items) {
+        this.items = items;
+    }
+
+    public GridPane() {
+    }
+
+    public ObservableList<GirdColumn<DataType, NodeType>> getColumns() {
+        return columns;
+    }
+
+}

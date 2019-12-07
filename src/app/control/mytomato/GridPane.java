1d900217cffce3a2a22ef48f0e84db34ea129f3f
心如止水 <zen96285@gmail.com>
2019-12-07 11:25:26 +0800

    GridPane端写好了NameColumn列的直接调用

--- src/app/control/mytomato/GridPane.java
+++ src/app/control/mytomato/GridPane.java
@@ -1,7 +1,10 @@
 package app.control.mytomato;
 
+import app.control.GirdColumn;
+import app.control.GirdColumnFactory;
 import app.model.TomatoTask;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
@@ -11,11 +14,18 @@ public class GridPane extends app.control.GridPane<TomatoTask,Text> {
     private ArrayList<Text> nameTextList = new ArrayList<>();
     private ArrayList<Text> startTextList = new ArrayList<>();
     private ArrayList<Text> endTextList = new ArrayList<>();
+    private GirdColumn<TomatoTask, Text> nameColumn;
 
     public void setItems(ObservableList<TomatoTask> items) {
         this.items = items;
+        
+        nameColumn = new GirdColumn<>("name");
+        nameColumn.setNodeFactory(data -> new Text(data.getName()));
+        this.getColumns().add(nameColumn);
+
         createText();
     }
+
 
     private void createText() {
         createStartText();

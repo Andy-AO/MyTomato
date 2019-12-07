811d46a7ea1fc1a8bb6914067706a0c44b934d7b
心如止水 <zen96285@gmail.com>
2019-12-07 11:42:27 +0800

    删除掉多余的Start和End,准备采用GridColumn的方式实现

--- src/app/control/mytomato/GridPane.java
+++ src/app/control/mytomato/GridPane.java
@@ -10,9 +10,6 @@ import javafx.scene.text.Text;
 import java.util.ArrayList;
 
 public class GridPane extends app.control.GridPane<TomatoTask,Text> {
-
-    private ArrayList<Text> startTextList = new ArrayList<>();
-    private ArrayList<Text> endTextList = new ArrayList<>();
     private GirdColumn<TomatoTask, Text> nameColumn;
 
     public void setItems(ObservableList<TomatoTask> items) {
@@ -23,45 +20,6 @@ public class GridPane extends app.control.GridPane<TomatoTask,Text> {
 
         super.setItems(items);
 
-        createText();
-    }
-
-
-    private void createText() {
-        createStartText();
-        createEndText();
-    }
-
-    private void createStartText() {
-        createStartTextList();
-        int rowIndex = 0;
-        for (Text text : startTextList) {
-            this.add(text,1,rowIndex++);
-        }
-    }
-
-    private void createEndText() {
-        createEndTextList();
-        int rowIndex = 0;
-        for (Text text : endTextList) {
-            this.add(text,2,rowIndex++);
-        }
-    }
-
-    private void createStartTextList() {
-        for (TomatoTask tomatoTask : items) {
-            startTextList.add(new Text(tomatoTask.getStartTimeString()));
-        }
-    }
-
-    private void createEndTextList() {
-        for (TomatoTask tomatoTask : items) {
-            endTextList.add(new Text(tomatoTask.getEndTimeString()));
-        }
-    }
-
-    public ObservableList<TomatoTask> getItems() {
-        return items;
     }
 
 

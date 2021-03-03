717723b63291fb4b1073c7f529aa8ab33565eb13
zen96285 <zen96285@gmail.com>
2021-03-03 13:50:35 +0800

    src/test/OldPlay.java

---
+++ src/test/OldPlay.java
@@ -0,0 +1,14 @@
+package test;
+
+import app.Main;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class OldPlay {
+
+    public static void main(String[] args) {
+        Main.WORK_DURATION_MP3_PLAYER.repeatPlayInNewThread();
+    }
+}
